7b56516fb06bb9b09fb2dc2af734d3679a61e206
fabianvdW <fabianvonderwarth@gmail.com>
2019-07-16 14:13:24 +0200

    Actually set Version number to current version number

--- src/uci/uci_engine.rs
+++ src/uci/uci_engine.rs
@@ -9,7 +9,7 @@ pub struct UCIEngine<'a> {
 impl<'a> UCIEngine<'a> {
     pub fn standard() -> UCIEngine<'a> {
         UCIEngine {
-            name: &"FabChess v1.0",
+            name: &"FabChess v1.6",
             author: &"Fabian von der Warth",
             internal_state: GameState::standard(),
         }
