478e5f6d452e5edf07747fe671e70ef8e9e94e94
fabianvdW <fabianvonderwarth@gmail.com>
2019-07-21 20:51:34 +0200

    Implemente SGD for k tuning

--- src/tuning/tuning.rs
+++ src/tuning/tuning.rs
@@ -1,9 +1,12 @@
 extern crate core;
+extern crate rand;
+
 use core::evaluation::eval_game_state;
 use core::tuning::loading::{load_positions, FileFormatSupported, LabelledGameState, Statistics};
+use rand::{seq::SliceRandom, thread_rng};
 pub const POSITION_FILE: &str = "D:/FenCollection/Real/all_positions_qsearch.txt";
 //pub const POSITION_FILE: &str = "D:/FenCollection/Test/all_positions_qsearch.txt";
-
+const BATCH_SIZE: usize = 8196;
 pub fn main() {
     if !cfg!(feature = "texel-tuning") {
         panic!("Feature texel-tuning has to be enabled");
@@ -23,12 +26,12 @@ pub fn main() {
         positions.len()
     );
     let mut tuner = Tuner {
-        k: 0.69,
+        k: 0.624,
         positions: init_texel_states(positions),
     };
     println!("Started Tuning!");
     minimize_evaluation_error_fork(&mut tuner);
-    println!("{}", tuner.k);
+    println!("Optimal K: {}", tuner.k);
 }
 pub fn init_texel_states(labelledstates: Vec<LabelledGameState>) -> Vec<TexelState> {
     let mut res: Vec<TexelState> = Vec::with_capacity(labelledstates.len());
@@ -46,6 +49,9 @@ pub struct Tuner {
     pub k: f64,
     pub positions: Vec<TexelState>,
 }
+pub fn shuffle_positions(tuner: &mut Tuner) {
+    tuner.positions.shuffle(&mut thread_rng());
+}
 pub fn average_evaluation_error(tuner: &Tuner) -> f64 {
     let mut res = 0.;
     for pos in &tuner.positions {
@@ -53,37 +59,45 @@ pub fn average_evaluation_error(tuner: &Tuner) -> f64 {
     }
     res / tuner.positions.len() as f64
 }
+
 pub fn minimize_evaluation_error_fork(tuner: &mut Tuner) -> f64 {
     let mut best_k = tuner.k;
     let mut best_error = average_evaluation_error(&tuner);
     println!("Erorr in epoch 0: {}", best_error);
     let mut epoch = 0;
-    let mut lr = 0.4;
+    let mut lr = 0.05;
     loop {
         epoch += 1;
-        if lr <= 0.001 {
-            break;
-        }
+        //Shuffle positions
+        shuffle_positions(tuner);
         //Calculate dE/dk
-        let mut dedk = 0.;
-        for pos in &tuner.positions {
-            let eval = pos.eval;
-            dedk += (pos.lgs.label - sigmoid(tuner.k, eval)) * dsigmoiddk(tuner.k, eval);
-        }
-        dedk *= -2.0 / tuner.positions.len() as f64;
-        tuner.k += -lr * dedk;
-        let error = average_evaluation_error(&tuner);
-        if error < best_error {
-            if 0.00001 > best_error - error {
-                lr /= 2.0;
+        for batch in 0..(tuner.positions.len() - 1) / BATCH_SIZE + 1 {
+            let from = batch * BATCH_SIZE;
+            let mut to = (batch + 1) * BATCH_SIZE;
+            if to > tuner.positions.len() {
+                to = tuner.positions.len();
             }
+            let mut dedk = 0.;
+            for pos in &tuner.positions[from..to] {
+                let eval = pos.eval;
+                dedk += (pos.lgs.label - sigmoid(tuner.k, eval)) * dsigmoiddk(tuner.k, eval);
+            }
+            dedk *= -2.0 / (to - from) as f64;
+            tuner.k += -lr * dedk;
+        }
+
+        let error = average_evaluation_error(&tuner);
+        println!("Error in epoch {}: {}", epoch, error);
+        if error < best_error {
             best_error = error;
             best_k = tuner.k;
         } else {
             lr /= 2.0;
             tuner.k = best_k;
         }
-        println!("Error in epoch {}: {}", epoch, error);
+        if lr <= 0.001 {
+            break;
+        }
     }
     best_k
 }
