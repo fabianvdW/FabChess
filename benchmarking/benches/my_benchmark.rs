f7bd74f9247bd2ac04d599511bcaccd56af37471
fabianvdW <fabianvonderwarth@gmail.com>
2020-05-02 17:14:15 +0200

    Add criterion benchmarks

---
+++ benchmarking/benches/my_benchmark.rs
@@ -0,0 +1,40 @@
+use benchmarking::*;
+use core_sdk::board_representation::game_state_attack_container::GameStateAttackContainer;
+use core_sdk::evaluation::eval_game_state;
+use core_sdk::move_generation::movegen::{self, MoveList};
+use criterion::{criterion_group, criterion_main, Criterion};
+
+pub fn evaluation_bench(c: &mut Criterion) {
+    let states = load_benchmarking_positions();
+    let mut attack_container = GameStateAttackContainer::default();
+    c.bench_function("evaluation", |b| {
+        b.iter(|| {
+            let mut sum = 0;
+            for i in 0..BENCHMARKING_POSITIONS_AMOUNT {
+                attack_container.write_state(&states[i]);
+                sum += eval_game_state(&states[i], &attack_container, -16000, 16000).final_eval
+                    as isize;
+            }
+            sum
+        })
+    });
+}
+
+pub fn generate_moves_bench(c: &mut Criterion) {
+    let states = load_benchmarking_positions();
+    let mut attack_container = GameStateAttackContainer::default();
+    let mut movelist = MoveList::default();
+    c.bench_function("movegen", |b| {
+        b.iter(|| {
+            let mut sum = 0;
+            for i in 0..BENCHMARKING_POSITIONS_AMOUNT {
+                attack_container.write_state(&states[i]);
+                movegen::generate_moves(&states[i], false, &mut movelist, &attack_container);
+                sum += movelist.move_list.len();
+            }
+            sum
+        })
+    });
+}
+criterion_group!(benches, evaluation_bench, generate_moves_bench);
+criterion_main!(benches);
---
+++ benchmarking/src/lib.rs
@@ -0,0 +1,37 @@
+use core_sdk::board_representation::game_state::GameState;
+use extended_sdk::openings::load_db_until;
+use rand::Rng;
+use std::fs;
+
+pub const BENCHMARKING_POSITIONS: &str = "benchmarking_positions.txt";
+pub const BENCHMARKING_POSITIONS_AMOUNT: usize = 1000;
+pub const MAKE_BENCHMARKING_POSITIONS_FROM: &str = "./O-Deville/o-deville.pgn";
+pub const LOAD_DB_UNTIL: usize = 40;
+
+//************************************************************
+//* Benchmarking positions are just some random 100 positions from o-deville database loaded until 40th ply
+//*
+//************************************************************
+pub fn make_benchmarking_positions() {
+    let mut states: Vec<GameState> =
+        load_db_until(MAKE_BENCHMARKING_POSITIONS_FROM, LOAD_DB_UNTIL).0;
+    let mut rng = rand::thread_rng();
+    let mut write_str = String::new();
+    for _ in 0..BENCHMARKING_POSITIONS_AMOUNT {
+        let index = rng.gen_range(0, states.len());
+        let state = states.remove(index);
+        write_str.push_str(&format!("{}\n", state.to_fen()));
+    }
+    fs::write(BENCHMARKING_POSITIONS, write_str).expect("Unable to write file!");
+}
+
+pub fn load_benchmarking_positions() -> Vec<GameState> {
+    let mut states = Vec::with_capacity(BENCHMARKING_POSITIONS_AMOUNT);
+    let positions =
+        fs::read_to_string(BENCHMARKING_POSITIONS).expect("Unable to read benchmarking positions");
+    let new_linesplit = positions.split("\n").collect::<Vec<&str>>();
+    for i in 0..BENCHMARKING_POSITIONS_AMOUNT {
+        states.push(GameState::from_fen(new_linesplit[i]));
+    }
+    states
+}
