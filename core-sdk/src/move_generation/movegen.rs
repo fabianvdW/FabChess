892e1ba352461f1520660aa4801af184959bfc0a
fabianvdW <fabianvonderwarth@gmail.com>
2020-06-01 22:51:49 +0200

    Add kogge stone code, speeup setwise attacks a bit with kogge stone

--- core-sdk/src/move_generation/movegen.rs
+++ core-sdk/src/move_generation/movegen.rs
@@ -4,7 +4,10 @@ use crate::bitboards::bitboards::constants::{
     square, BISHOP_RAYS, FREEFIELD_BISHOP_ATTACKS, FREEFIELD_ROOK_ATTACKS, KING_ATTACKS,
     KNIGHT_ATTACKS, RANKS, ROOK_RAYS,
 };
-use crate::bitboards::bitboards::square;
+use crate::bitboards::bitboards::{
+    east_attacks, noea_attacks, north_attacks, nowe_attacks, soea_attacks, south_attacks,
+    sowe_attacks, square, west_attacks,
+};
 use crate::board_representation::game_state::{
     GameMove, GameMoveType, GameState, PieceType, WHITE,
 };
@@ -19,15 +22,12 @@ impl GameState {
         } else {
             self.get_all_pieces_without_ctm_king()
         };
-        let mut res = 0u64;
-        //TODO Kogge Stone
-        for pt in [PieceType::Rook, PieceType::Queen].iter() {
-            let mut piece = self.get_piece(*pt, side);
-            while piece > 0 {
-                let idx = piece.trailing_zeros() as usize;
-                res |= (*pt).attacks(idx, occupied_squares);
-                piece ^= square(idx);
-            }
+        let mut res = rook_kogge_stone(self.get_rook_like_bb(side), occupied_squares);
+        let mut queens = self.get_piece(PieceType::Queen, side);
+        while queens > 0 {
+            let idx = queens.trailing_zeros() as usize;
+            res |= PieceType::Bishop.attacks(idx, occupied_squares);
+            queens ^= square(idx);
         }
         res
     }
@@ -128,6 +128,20 @@ impl PieceType {
             _ => panic!("Pawn is not supported due to branching!"),
         }
     }
+}
+#[inline(always)]
+pub fn bishop_kogge_stone(bishops: u64, all_pieces: u64) -> u64 {
+    nowe_attacks(bishops, all_pieces)
+        | noea_attacks(bishops, all_pieces)
+        | sowe_attacks(bishops, all_pieces)
+        | soea_attacks(bishops, all_pieces)
+}
+#[inline(always)]
+pub fn rook_kogge_stone(rooks: u64, all_pieces: u64) -> u64 {
+    north_attacks(rooks, all_pieces)
+        | south_attacks(rooks, all_pieces)
+        | west_attacks(rooks, all_pieces)
+        | east_attacks(rooks, all_pieces)
 }
 #[inline(always)]
 pub fn bishop_attack(square: usize, all_pieces: u64) -> u64 {
