fa3c7fb1ac1a56d196bf8d6815dba765cd3d7fab
fabianvdW <fabianvonderwarth@gmail.com>
2020-09-16 15:49:05 +0200

    Another attempt
    Full tune starting at base constants, fixed attack values.
    BENCH=12676057

--- tuning/src/main.rs
+++ tuning/src/main.rs
@@ -24,32 +24,6 @@ pub fn main() {
         positions,
         params: Parameters::default(),
     };
-    println!("Error before: {}", average_evaluation_error(&tuner));
-    tuner.params.special[IDX_KNIGHT_ATTACK_VALUE] = 4.;
-    tuner.params.special[IDX_KNIGHT_ATTACK_VALUE + 1] = 5.;
-
-    tuner.params.special[IDX_BISHOP_ATTACK_VALUE] = 4.;
-    tuner.params.special[IDX_BISHOP_ATTACK_VALUE + 1] = 5.;
-
-    tuner.params.special[IDX_ROOK_ATTACK_VALUE] = 6.;
-    tuner.params.special[IDX_ROOK_ATTACK_VALUE + 1] = 5.;
-
-    tuner.params.special[IDX_QUEEN_ATTACK_VALUE] = 8.;
-    tuner.params.special[IDX_QUEEN_ATTACK_VALUE + 1] = 5.;
-
-    tuner.params.special[IDX_KNIGHT_CHECK_VALUE] = 8.;
-    tuner.params.special[IDX_KNIGHT_CHECK_VALUE + 1] = 10.;
-
-    tuner.params.special[IDX_BISHOP_CHECK_VALUE] = 8.;
-    tuner.params.special[IDX_BISHOP_CHECK_VALUE + 1] = 10.;
-
-    tuner.params.special[IDX_ROOK_CHECK_VALUE] = 10.;
-    tuner.params.special[IDX_ROOK_CHECK_VALUE + 1] = 10.;
-
-    tuner.params.special[IDX_QUEEN_CHECK_VALUE] = 12.;
-    tuner.params.special[IDX_QUEEN_CHECK_VALUE + 1] = 10.;
-    update_evaluations(&mut tuner);
-    println!("Error after: {}", average_evaluation_error(&tuner));
     println!("Start tuning for k");
     if OPTIMIZE_K {
         minimize_evaluation_error_fork(&mut tuner);
