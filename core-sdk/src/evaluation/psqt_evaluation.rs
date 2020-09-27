a3d2202b4a40fba92cff3bc78e6e804940570f05
TerjeKir <42723273+TerjeKir@users.noreply.github.com>
2020-09-27 20:39:19 +0200

    Various cleanup of psqt function (#75)
    No functional change
    BENCH=8851108

--- core-sdk/src/evaluation/psqt_evaluation.rs
+++ core-sdk/src/evaluation/psqt_evaluation.rs
@@ -4,35 +4,36 @@ use crate::board_representation::game_state::{GameState, PieceType, PIECE_TYPES,
 use crate::evaluation::params::PSQT;
 
 #[cfg(feature = "texel-tuning")]
-use crate::bitboards::bitboards::mirror_square;
+use crate::bitboards::bitboards::white_pov;
 #[cfg(feature = "texel-tuning")]
 use crate::evaluation::parameters::normal_parameters::IDX_PSQT;
 #[cfg(feature = "texel-tuning")]
 use crate::evaluation::trace::LargeTrace;
 
 pub fn psqt(game_state: &GameState, side: usize, #[cfg(feature = "texel-tuning")] trace: &mut LargeTrace) -> EvaluationScore {
-    let mut res = EvaluationScore::default();
     #[cfg(feature = "display-eval")]
     {
         println!("\nPSQT for {}:", if side == WHITE { "White" } else { "Black" });
     }
+
+    let mut res = EvaluationScore::default();
+
     for &pt in PIECE_TYPES.iter() {
+
         let mut piece_sum = EvaluationScore::default();
         let mut piece = game_state.get_piece(pt, side);
+
         while piece > 0 {
-            #[allow(unused_mut)]
-            let mut idx = piece.trailing_zeros() as usize;
+            let idx = piece.trailing_zeros() as usize;
             piece ^= square(idx);
             piece_sum += PSQT[pt as usize][side][idx / 8][idx % 8] * if side == WHITE { 1 } else { -1 };
             #[cfg(feature = "texel-tuning")]
             {
-                if side != WHITE {
-                    idx = mirror_square(idx);
-                }
-                trace.normal_coeffs[IDX_PSQT + 64 * pt as usize + idx] += if side == WHITE { 1 } else { -1 };
+                trace.normal_coeffs[IDX_PSQT + 64 * pt as usize + white_pov(idx, side)] += if side == WHITE { 1 } else { -1 };
             }
         }
         res += piece_sum;
+
         #[cfg(feature = "display-eval")]
         {
             println!("\t{:?}  : {}", pt, piece_sum);
