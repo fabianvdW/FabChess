4fa191c51ef2848a9be4fe2faed086cfade254a0
fabianvdW <fabianvonderwarth@gmail.com>
2021-01-12 22:19:40 +0100

    More tune
    BENCH=8368395

--- core-sdk/src/search/alphabeta.rs
+++ core-sdk/src/search/alphabeta.rs
@@ -26,9 +26,9 @@ pub const STATIC_NULL_MOVE_DEPTH: i16 = 5;
 pub const NULL_MOVE_PRUNING_DEPTH: i16 = 3;
 pub const HISTORY_PRUNING_DEPTH: i16 = 2;
 pub const HISTORY_PRUNING_THRESHOLD: isize = 0;
-pub const SEE_PRUNING_DEPTH: i16 = 8;
-pub const SEE_PRUNING_CAPTURE_MULT: f64 = -50.;
-pub const SEE_PRUNING_QUIET_MULT: f64 = -3.;
+pub const SEE_PRUNING_DEPTH: i16 = 11;
+pub const SEE_PRUNING_CAPTURE_MULT: f64 = -48.;
+pub const SEE_PRUNING_QUIET_MULT: f64 = -7.;
 
 pub fn principal_variation_search(mut p: CombinedSearchParameters, thread: &mut Thread) -> i16 {
     //Step 0. Prepare variables
