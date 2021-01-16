6435a367e0d9e2937e2f2026bfed2ff41bc97550
fabianvdW <fabianvonderwarth@gmail.com>
2021-01-16 22:14:53 +0100

    Fix a rare occurences of crashes
    Crashes occured in high throughput searches(within microseconds) and due to locks being locks.

--- core-sdk/src/search/searcher.rs
+++ core-sdk/src/search/searcher.rs
@@ -120,9 +120,7 @@ impl InterThreadCommunicationSystem {
     }
 
     pub fn get_time_elapsed(&self) -> u64 {
-        let now = Instant::now();
-        let dur = now.duration_since(*self.start_time.read().unwrap());
-        dur.as_millis() as u64
+        *self.start_time.read().unwrap().elapsed().as_millis() as u64
     }
 
     pub fn update(&self, thread_id: usize, nodes_searched: u64, seldepth: usize) {
